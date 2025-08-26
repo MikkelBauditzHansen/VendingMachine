d5cf4315742b554db92716ee9542c2f59c986f17
Mikkel Bauditz Hansen <mih016@edu.zealand.dk>
2025-08-26 13:13:44 +0200

    iop

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej med dig jeg hedder kaj!");
+            Console.WriteLine("Hej med dig jeg hedder andrea!");
         }
     }
 }
